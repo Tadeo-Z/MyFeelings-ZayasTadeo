26bb67eacca21b27236a5d6e587eee28ff836b6c
Tadeo-Z <fernando.zayas228310@potros.itson.edu.mx>
2024-06-24 23:28:05 -0700

    Initial commit

---
+++ app/src/main/java/zayas/tadeo/myfeelings/utilities/Emociones.kt
@@ -0,0 +1,6 @@
+package zayas.tadeo.myfeelings.utilities
+
+data class Emociones(var nombre: String,
+                     var porcentaje: Float,
+                     var color: Int,
+                     var total: Float)
